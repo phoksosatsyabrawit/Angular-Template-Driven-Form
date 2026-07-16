d926d7612f9ba77eb3465b99b6713b9722facee3
local.brawit <phoksosatsyabrawit@gmail.com>
2026-07-16 09:45:22 +0700

    form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
+import { Student } from './models/student';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-form');
+
+  subjects = [
+    'Java', 'C#', 'TypeScript', 'jQuery', 'Angular'
+  ];
+
+  student = new Student('steve', 'steve@gamil.com', 'male', 'Java');
 }
